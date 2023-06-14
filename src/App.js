bf25533dfbe17512b654743cd1fc56a34893a6d5
Marvis Knight <knightmar78@gmail.com>
2023-06-14 07:37:53 -0700

    routes practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Routes,
+  Route,
+  Link 
+} from "react-router-dom";
+    
+const Location = (props) => {
 
+  return (
+    <div>
+        <h1 style={{color: "red"}}>Location Component Loaded!</h1>
+    </div>
+  );
+}
+    
+
+    
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Location</h1>
+      <p>
+        <Link to="/location/seattle">Seattle</Link>
+        |
+        <Link to="/location/chicago">Chicago</Link>
+        |
+        <Link to="/location/burbank">Burbank</Link>
+      </p>
+      <Routes>
+        <Route path="/location/:city" element={<Location />} />
+      </Routes>
     </div>
   );
 }
-
-export default App;
+    
+export default App
